f28230b46026c4e65b15f02e996f3b6f4a764183
ShanYu Er <ershanyu1513@gmail.com>
2026-03-08 23:06:17 +0800

    modified part 2

--- Lab 1/dfs.py
+++ Lab 1/dfs.py
@@ -2,7 +2,7 @@ from heapq import heappop, heappush, heapify
 import json
 
 with open('G.json', 'r') as file:
-    G_details = json.load(file)
+    G_node = json.load(file)
 
 with open('Coord.json', 'r') as file:
     coords = json.load(file)
@@ -10,52 +10,35 @@ with open('Coord.json', 'r') as file:
 with open('Dist.json', 'r') as file:
     distpair = json.load(file)
 
+with open('Cost.json', 'r') as file:
+    cost = json.load(file)
+
 def dfs(maxenergy):
     start, end = '1', '50'
-    distances = {}
-    energy = {}
-    prev = {}
-    distances["1"] = 0
-    energy["1"] = 0
-    current = '0'
-    nextqueue = [(0, "0,1")]
-    visited = []
-    i = 0
-    prev['1'] = 0
-    while current != end and len(nextqueue) != 0:
-        newdist, pair = heappop(nextqueue)
-        current, next = pair.split(",")
-        #print(f"newdist = {newdist} and current = {current} and next = {next}")
-        if next not in visited:
-            visited.append(next)
-            current = next
-            neighbouring = G_details[current]
-            d = distances[current]
-            for x in neighbouring:
-                if x in visited:
-                    continue
-                pair = current + "," + x
-                #print(f"{pair} and distance: {distpair[pair]}")
-                if x not in distances:
-                    distances[x] = d+distpair[pair]
-                    heappush(nextqueue, (distpair[pair], pair))
-                    prev[x] = current
-                elif d + distpair[pair] < distances[x]:
-                    distances[x] = d+distpair[pair]
-                    prev[x] = current
-            print(current)
+    totaldist = {start: 0}
+    energy = {start: 0}
+    parent = {}
+    nextqueue = []
+    heappush(nextqueue, (0, start))
+    while len(nextqueue) != 0:
+        accdist, cur = heappop(nextqueue)
+        if cur == end:
+            break
+        for node in G_node[cur]:
+            pair = cur + "," + node
+            if cost[pair] + energy[cur] > maxenergy:
+                continue
+            elif node not in totaldist or distpair[pair] + accdist < totaldist[node]:
+                totaldist[node] = distpair[pair] + accdist
+                energy[node] = cost[pair] + energy[cur]
+                heappush(nextqueue, (totaldist[node], node))
 
-    print(distances["2"])
-    return prev, distances["50"]
+    print(totaldist["2"])
+    print(f"Energy = {energy["50"]}")
+    return totaldist["50"]
+        
 
-
-def printpath(prev, node):
-    if prev[node] == 0:
-        return node
-    else:
-        return printpath(prev, prev[node]) + "->" + (node)
 
 energy = 287932
-prev, w = dfs(energy)
+w = dfs(energy)
 print(w)
-print(printpath(prev, '50'))
